754137faedc7f50ab83da5646f3173b04ff4fbe0
karenok <karenokonkwo29@gmail.com>
2020-08-02 15:34:47 +0000

    test and implement endpoint to get categories

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -35,6 +35,10 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_get_categories_success(self):
+        res = self.client().get("/categories")
+        self.assertEqual(res.status_code, 200)
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
