9d7706c7b08b901c7257d535d12ba617865c750d
karenok <karenokonkwo29@gmail.com>
2020-08-03 09:29:01 +0000

    style changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,46 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../stylesheets/Header.css';
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import "../stylesheets/Header.css";
 
 class Header extends Component {
+	navTo(uri) {
+		window.location.href = window.location.origin + uri;
+	}
 
-  navTo(uri){
-    window.location.href = window.location.origin + uri;
-  }
-
-  render() {
-    return (
-      <div className="App-header">
-        <h1 onClick={() => {this.navTo('')}}>Udacitrivia</h1>
-        <h2 onClick={() => {this.navTo('')}}>List</h2>
-        <h2 onClick={() => {this.navTo('/add')}}>Add</h2>
-        <h2 onClick={() => {this.navTo('/play')}}>Play</h2>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App-header">
+				<h1
+					onClick={() => {
+						this.navTo("");
+					}}
+				>
+					Udacitrivia
+				</h1>
+				<h2
+					onClick={() => {
+						this.navTo("");
+					}}
+				>
+					List
+				</h2>
+				<h2
+					onClick={() => {
+						this.navTo("/add");
+					}}
+				>
+					Add
+				</h2>
+				<h2
+					onClick={() => {
+						this.navTo("/play");
+					}}
+				>
+					Play
+				</h2>
+			</div>
+		);
+	}
 }
 
 export default Header;
